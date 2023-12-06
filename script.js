e52e40e41e8f1f6ba952143d5ceec26078db3359
Ane Fernandes <elianepatricio@hotmail.com.br>
2023-12-06 20:22:14 -0300

    desafio logica js

---
+++ script.js
@@ -0,0 +1,46 @@
+
+// Mostre no console.log a mensagem "Ano novo está chegando!!";
+let mensagem = "Ano novo está chegando!!"
+console.log(mensagem);
+
+
+// Crie uma variável que receba o nome da cantora Mariah Carey
+// Crie uma variável que receba a idade dela
+// Crie uma variável com o ano atual e subtraia pelo o valor da idade da Mariah Carey
+let nome1 = "Mariah Carey"
+let idade = "32"
+let ano = 2023 - idade
+console.log(ano);
+
+// Crie uma variável para guardar a data de nascimento da Mariah Carey
+let nascimento = ano
+console.log(nascimento); // Mostre no console a data de nascimento da Mariah Carey
+
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console.log
+let quartaFeira = true
+console.log(quartaFeira);
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
+
+// Crie uma condição  SE você for maior ou igual 18 retornará  a mensagem:  Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey. SENÃO for maior ou igual a 18,  retorne a seguinte mensagem:  "Sinto muito, mas assistirá da tv globo"
+let idade1 = 12
+if (idade1 >=18){
+    console.log("Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey");
+}
+else {
+    console.log("Sinto muito, mas assistirá da tv globo");
+}
+
+// Crie uma condição que exiba uma mensagem no console se você fizer aniversário em Agosto OU Dezembro OU JUNHO.
+let mesAniversario = "setembro"
+if (mesAniversario == "agosto" || mesAniversario == "dezembro" || mesAniversario == "junho"){
+    console.log(`Uma ou mais opções estão corretas. O mês escolhido foi o mês ${mesAniversario}`);
+}
+else {
+    console.log(`Algo de errado não está certo, o mês digitado foi o mês ${mesAniversario}.`);
+}
+
+// Aluna
+let aluna = "eliane patricio"
+console.log(`Olá sou ${aluna}, aluna VNW`);
